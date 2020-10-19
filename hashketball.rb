499d143713833c18fd8c67e2f9c87c23b29e9d1c
Brodrick George <brodrick.george@yahoo.com>
2020-10-19 07:12:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,10 +1,9 @@
 def game_hash(data)
-  {
+  epic_game = {
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players: [
-        {
+      players: [{
           player_name: "Alan Anderson",
           number: 0,
           shoe: 16,
